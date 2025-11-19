1d0497e83be5d4c59db77454c6a166fc63a129bf
atharvjadhav215 <168112689+atharvjadhav215@users.noreply.github.com>
2025-11-19 14:05:44 +0530

    Created Registration Page

--- src/frontend/src/components/Header.tsx
+++ src/frontend/src/components/Header.tsx
@@ -30,6 +30,7 @@ export default function Header() {
     { name: "Idea", href: "/submit" },
     { name: "Events", href: "/events" },
     { name: "Leaderboard", href: "/leaderboard" },
+    { name: "Registration", href: "/registration" },
   ];
 
   return (
